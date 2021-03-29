53c7644a14f7d96ea8047058d4a0170a8a205670
ryzecous <r3j9c89@gmail.com>
2021-03-29 15:58:12 -0400

    creating components needed for project - bringing components into App.js - installing axios and styled-components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import logo from './logo.svg';
+import { useState } from 'react'
 import './App.css';
 
+// importing components
+import Avatar from './components/Avatar'
+import Details from './components/Details'
+import Filter from './components/Filter'
+import Header from './components/Header'
+import Sort from './components/Sort'
+
+
 function App() {
+  const [user, setUser] = useState({})
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Header />
+        <Sort />
+        <Filter />
+        <Avatar />
+        <Details />
       </header>
     </div>
   );
