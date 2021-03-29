d54152c37361a2fd069bf7dbb29cf1c35449e050
ryzecous <r3j9c89@gmail.com>
2021-03-29 16:48:32 -0400

    creating a 'location' menu

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import Filter from './components/Filter'
 import Header from './components/Header'
 import Sort from './components/Sort'
 
+// https://randomuser.me/api/?results=25
+// this is the api call for 25 random users
 
 function App() {
   const [user, setUser] = useState({})
@@ -17,15 +19,15 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <header className="App-header">
-        <Sort />
-        <Filter 
-          filter = {filter} 
-          setFilter = {setFilter}
-        />
-        <Avatar />
-        <Details />
-      </header>
+        <header className="App-header">
+          <Sort />
+          <Filter 
+            filter = {filter} 
+            setFilter = {setFilter}
+          />
+          <Avatar />
+          <Details />
+        </header>
     </div>
   );
 }
--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,10 +1,60 @@
-import React from 'react'
+import React, { Component } from 'react'
 import styled from 'styled-components'
 
-const Filter = ({ filter, setFilter }) => {
-    return <div>
-        Filter
-    </div>
+class Filter extends Component {
+    constructor() {
+        super();
+        
+        this.state = {
+          showMenu: false,
+        }
+
+        this.showMenu = this.showMenu.bind(this);
+      }
+
+      showMenu(event) {
+          event.preventDefault();
+
+          this.setState({
+              showMenu: true,
+          })
+      }
+
+    render() {
+        return (
+          <div>
+            <button onClick={this.showMenu}>
+              Location
+            </button>
+
+            {
+                this.state.showMenu
+                ? (
+                <div className="menu">
+                    <button> Local </button>
+                    <button> Remote </button>
+                </div>
+                )
+                : (
+                    null
+                )
+            }
+
+          </div>
+        );
+      }
 }
+
+// const Filter = ({ filter, setFilter }) => {
+//     return <div className="dd-wrapper">
+//     <div className="dd-header">
+//       <div className="dd-header-title">Location</div>
+//     </div>
+//     <div className="dd-list">
+//       <button className="dd-list-item">Local</button>
+//       <button className="dd-list-item">Remote</button>
+//     </div>
+//   </div>
+// }
 
 export default Filter
